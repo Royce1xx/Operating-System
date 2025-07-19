873063a450aa06284d3c2690a3e7bf73f8152ef1
Eugene1x <roycekinchloe@gmail.com>
2025-07-19 02:30:37 -0400

    Part of my asm entry

--- kernel.asm
+++ kernel.asm
@@ -1,5 +1,23 @@
-16 bits
-org 0x1000
+[BITS 32]              ; We are in 32-bit mode
+[GLOBAL _start]        ; Make _start visible to the linker this why its global
 
-start:
-    cli ; disables interrupts 
+
+_start:
+    cli                 ; pause the interrpts i lowkey dont know 
+
+    ; Setting all segments to 0x10 
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax          
+    mov gs, ax
+    mov ss, ax
+
+
+    mov esp, 0x9FB00       ; Setting stack pointer to safe adress because stack grows down so its set at a high adress
+
+    call main             ; main function inside of our c file 
+
+
+.hang:
+    jmp .hang              ; This makes an infite loop
